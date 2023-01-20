6ba16833088ffd8d840c33003df33691c407f384
Hamisi Rawlins <hamisirawlins@Hamisis-MacBook-Air.local>
2023-01-20 13:10:13 +0300

    Initialize app + folder structure

--- server/index.js
+++ server/index.js
@@ -8,8 +8,48 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from "path";
 import { fileURLToPath } from 'url';
+import {register } from "./controllers/auth.js"
 
 
 //Middleware and Packages Setup
 
-console.log("Test")
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+//Env
+dotenv.config()
+
+//Initialize App
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("common"))
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')))
+
+//File Storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+//Upload
+const upload = multer({ storage })
+
+
+//Routes with File needs
+app.post("/auth/register", upload.single("picture"), register)
+
+//Mongoose Setup
+const PORT = process.env.PORT || 6001
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
+}).catch((err) => console.log(`${err}, did not connect`))
